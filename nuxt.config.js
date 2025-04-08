3c27907b5dce5a8a7d7ff9dd16d48241fb3f0fff
nikhil rai <nikhil@skyaitechnologies.com>
2025-04-08 17:01:07 +0530

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,7 +74,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: 'https://backstage.cyber3ra.com',
-    baseURL: 'http://127.0.0.1:8000',  
+    baseURL: 'https://cyback.onrender.com',  
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
