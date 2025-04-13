dd7f7a75ad593139aab6ef62445a2fc4af52451e
nikhil rai <nikhil@skyaitechnologies.com>
2025-04-13 13:33:03 +0530

    dskjkjsdf

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,6 +74,14 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: 'http://127.0.0.1:8000',
+    // baseURL: 'http://127.0.0.1:8000',
+    // baseURL: 'http://127.0.0.1:8000',
+
+    // baseURL: 'http://127.0.0.1:8000',
+    // baseURL: 'http://127.0.0.1:8000',
+
+
+
     baseURL: 'https://cyback.onrender.com',  
   },
 
