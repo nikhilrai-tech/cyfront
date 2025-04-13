b70d87ff2a0523ed89550e3d56734379f56ed617
nikhil rai <nikhil@skyaitechnologies.com>
2025-04-13 14:01:19 +0530

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,12 +2,13 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'server',
+  target: 'static',
   
-  server : {
+  server: {
     port: 3500,
     host: "0.0.0.0"
   },
+  
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - SecuriumX',
@@ -18,10 +19,12 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/SECURIUM.svg'},
-      {rel: 'stylesheet', type: 'text/css', href: 'https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css'}
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/SECURIUM.svg' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css' }
     ],
   },
+  
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/main.css',
@@ -29,19 +32,14 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [ { src: '~/plugins/axios', ssr: true },{src: '~/plugins/notifier.js', ssr: false},{ src: '~/plugins/tiptap.js', mode: 'client' }, ], 
+  plugins: [
+    { src: '~/plugins/axios', ssr: true },
+    { src: '~/plugins/notifier.js', ssr: false },
+    { src: '~/plugins/tiptap.js', mode: 'client' },
+  ],
 
   router: {
     middleware: ['router-auth'],
-    // scrollBehavior(to) {
-    //   if (to.hash) {
-    //     return window.scrollTo({
-    //       top: document.querySelector(to.hash).offsetTop + window.innerHeight,
-    //       behavior: 'smooth'
-    //     })
-    //   }
-    //   return window.scrollTo({ top: 0, behavior: 'smooth' })
-    // }
   },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -49,40 +47,28 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/moment',
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/google-analytics',
     '@nuxtjs/vuetify',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     '@nuxtjs/auth-next'
   ],
+  
   loading: {
     color: '#58a6ff',
     height: '3px',
     duration: 3000
-  }, 
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    // baseURL: 'http://127.0.0.1:8000',
-    // baseURL: 'http://127.0.0.1:8000',
-    // baseURL: 'http://127.0.0.1:8000',
-
-    // baseURL: 'http://127.0.0.1:8000',
-    // baseURL: 'http://127.0.0.1:8000',
-
-
-
-    baseURL: 'https://cyback.onrender.com',  
+    baseURL: 'https://cyback.onrender.com',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
@@ -100,6 +86,7 @@ export default {
       purpose: []
     }
   },
+
   layoutTransition: {
     name: 'default',
     mode: 'out-in'
@@ -109,13 +96,13 @@ export default {
   vuetify: {
     treeShake: true,
     customVariables: ['~/assets/variables.scss'],
-    defaultAssets : {
+    defaultAssets: {
       font: {
-        family: 'Rajdhani' 
+        family: 'Rajdhani'
       },
     },
     theme: {
-      dark : true,
+      dark: true,
       themes: {
         options: { customProperties: true },
         dark: {
@@ -134,7 +121,6 @@ export default {
         },
         light: {
           primary: '#03CDD7',
-          // secondary: '#fb9678',
           secondary: '#8361da',
           accent: '#0040b9',
           info: '#4c98ef',
@@ -150,23 +136,19 @@ export default {
   build: {
     extractCSS: true,
     splitChunks: {
-       layouts: true
+      layouts: true
     },
-    publicPath: '/_cyber3ra/'
+    publicPath: '/_cyber3ra/',
+    buildDir: 'dist' // Set output directory
   },
+
   globalName: 'cyber3ra',
+
   googleAnalytics: {
-    // id: 'UA-163821965-4',
     id: 'UA-163821965-3',
-    // dev: true,
-    // debug: {
-    //     enabled: true,
-    //     sendHitTask: true
-    //   }
   },
 
   auth: {
-    // plugins: [ { src: '~/plugins/axios', ssr: true } ],
     strategies: {
       local: {
         token: {
@@ -182,10 +164,6 @@ export default {
           data: 'refresh_token',
           maxAge: 86400
         },
-        // user: {
-        //   property: 'user',
-        //   autoFetch: true
-        // },
         endpoints: {
           login: {
             url: "/app/accounts/login",
@@ -198,23 +176,11 @@ export default {
             propertyName: false,
           },
           tokenRequired: true,
-          // authorization: 'https://accounts.google.com/o/oauth2/auth',
-          // token: undefined,
-          // userInfo: 'https://www.googleapis.com/oauth2/v3/userinfo',
           logout: {
             url: "/app/accounts/logout",
             method: "get",
           }
         },
-        // token: {
-        //   property: 'access_token',
-        //   type: 'Bearer',
-        //   maxAge: 1800
-        // },
-        // refreshToken: {
-        //   property: 'refresh_token',
-        //   maxAge: 60 * 60 * 24 * 30
-        // },
         options: {
           expires: 1800
         }
